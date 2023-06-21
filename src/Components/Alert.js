b04715f0635406635598e60f296b3f55dedce9dd
Wasif007 <wasifateeq007@hotmail.com>
2023-06-21 19:40:00 +0500

    CLS improved in Alerts Notifications

--- src/Components/Alert.js
+++ src/Components/Alert.js
@@ -6,8 +6,10 @@ export default function Alert(props) {
  return lower.charAt(0).toUpperCase()+ lower.slice(1);
     }
   return (
+    <div style={{height:'50px'}}>{
    props.alert && <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
   <strong>{toUpperCaseFunction(props.alert.type)}</strong>: {props.alert.message} 
+</div>}
 </div>
   )
 }
